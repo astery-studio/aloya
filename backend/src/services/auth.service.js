58f7d844104c9471c703229aff6c3a1b18afc5f9
Carla Cristina <carlacristina130523@gmail.com>
2026-09-03 11:31:32 -0300

    feat(usuario): validar existencia de email antes do cadastro

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -7,4 +7,15 @@ const { calcularIdade } = require('../utils/date.utils');
 async function cadastrarUsuario(dados) {
     //Conversão do e-mail para letras minusculas
     const emailFormatado = dados.email.toLowerCase();
+
+    //verificando se o email já existe no banco
+    const usuarioExistente = await prisma.usuario.findUnique({
+        where: { email: emailFormatado }
+    });
+
+    if (usuarioExistente) {
+        throw new Error('Este e-mail já está em uso. Tente fazer login.');
+    }
+
+    
 }
