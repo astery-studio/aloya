fadff195130385c37b7c25f71408ff148ff9c8cf
Carla Cristina <carlacristina130523@gmail.com>
2026-09-06 01:39:41 -0300

    feat(app): inicializar servidor Express, middlewares de seguranca e rotas de autenticacao

---
+++ backend/src/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const { env } = require('./config/env');
+const { criarContainer } = require('./config/container');
+
+const {
+    criarAuthRoutes
+} = require('./routes/auth.routes');
+
+const {
+    rotaNaoEncontrada,
+    tratarErros
+} = require('./middlewares/error.middleware');
+
+const app = express();
+const container = criarContainer();
+
+app.disable('x-powered-by');
+
+app.use(helmet());
+
+app.use(
+    express.json({
+        limit: '32kb',
+        strict: true
+    })
+);
+
+app.use(
+    '/auth',
+    criarAuthRoutes(container.authController)
+);
+
+app.use(rotaNaoEncontrada);
+app.use(tratarErros);
+
+app.listen(env.port, () => {
+    console.log(`API executando na porta ${env.port}.`);
+});
