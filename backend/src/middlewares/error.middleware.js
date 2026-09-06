e7570163f02e2d8ad9798e6315a362bd121f0fee
Carla Cristina <carlacristina130523@gmail.com>
2026-09-06 01:32:10 -0300

    feat(error): implementar handler de erros globais para a aplicacao

--- backend/src/middlewares/error.middleware.js
+++ backend/src/middlewares/error.middleware.js
@@ -7,6 +7,20 @@ function rotaNaoEncontrada(_req, res) {
     });
     }
 
+    // Global de tratamento de erros da aplicação 
+    function tratarErros(erro, _req, res, _next) {
+        // Verifica se o objeto de erro possui um status HTTP customizado definido
+    if (erro.status) {
+        return res.status(erro.status).json({
+        erro: {
+            codigo: erro.codigo || 'ERRO_APLICACAO',
+            mensagem: erro.message
+        }
+        });
+    }
+}
+
 module.exports = {
     rotaNaoEncontrada,
+    tratarErros
 };
