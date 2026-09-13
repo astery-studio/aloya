3c1e39a330499327125b0afc83bb59e5a6a0a2de
Kaylanne <kaylannesatiro@icloud.com>
2026-09-13 19:16:05 -0300

    feat: add config prisma

---
+++ backend/src/config/prisma.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const {
+    PrismaBetterSqlite3
+} = require('@prisma/adapter-better-sqlite3');
+
+const {
+    PrismaClient
+} = require('../generated/prisma/client');
+
+function criarPrisma() {
+    if (!process.env.DATABASE_URL) {
+        throw new Error(
+            'A variável DATABASE_URL não foi configurada.'
+        );
+    }
+
+    const adapter = new PrismaBetterSqlite3({
+        url: process.env.DATABASE_URL
+    });
+
+    return new PrismaClient({
+        adapter
+    });
+}
+
+module.exports = {
+    criarPrisma
+};
