1cb30f8d2625208bc3a56e18cddc9212031b2588
Kaylanne <kaylannesatiro@icloud.com>
2026-09-14 18:20:40 -0300

    feat: theme color criado

---
+++ frontend/theme/colors.js
@@ -0,0 +1,27 @@
+const cores = Object.freeze({
+    marca: Object.freeze({
+        primaria: '#C85A44',
+        secundaria: '#2C4C3B'
+    }),
+
+    neutras: Object.freeze({
+        fundoClaro: '#F7F5F0',
+        superficieClara: '#FFFFFF',
+        textoPrincipalClaro: '#222222',
+        textoSecundarioClaro: '#5C5C59',
+        bordaClara: '#E6E2D8',
+
+        fundoEscuro: '#181C1A',
+        superficieEscura: '#232926',
+        textoPrincipalEscuro: '#EAE8E3'
+    }),
+
+    feedback: Object.freeze({
+        sucesso: '#3B8255',
+        aviso: '#D68C3A',
+        erro: '#B43D3D',
+        informacao: '#4A758E'
+    })
+})
+
+export { cores }
