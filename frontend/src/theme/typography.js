ade7c96b45141e2a376528576a8a900c0c19a38c
Carla Cristina <carlacristina130523@gmail.com>
2026-09-14 15:57:36 -0300

    feat(theme): adicionar tokens de tipografia

---
+++ frontend/src/theme/typography.js
@@ -0,0 +1,61 @@
+const fontFamilies = {
+    regular: 'DMSans_400Regular',
+    medium: 'DMSans_500Medium',
+    bold: 'DMSans_700Bold'
+};
+
+const typography = {
+    h1: {
+        fontFamily: fontFamilies.bold,
+        fontSize: 28,
+        fontStyle: 'normal',
+        fontWeight: '700',
+        lineHeight: 33.6
+    },
+
+    h2: {
+        fontFamily: fontFamilies.bold,
+        fontSize: 22,
+        fontStyle: 'normal',
+        fontWeight: '700',
+        lineHeight: 28.6
+    },
+
+    bodyLarge: {
+        fontFamily: fontFamilies.medium,
+        fontSize: 18,
+        fontStyle: 'normal',
+        fontWeight: '500',
+        lineHeight: 27
+    },
+
+    bodyDefault: {
+        fontFamily: fontFamilies.regular,
+        fontSize: 16,
+        fontStyle: 'normal',
+        fontWeight: '400',
+        lineHeight: 24
+    },
+
+    caption: {
+        fontFamily: fontFamilies.medium,
+        fontSize: 14,
+        fontStyle: 'normal',
+        fontWeight: '500',
+        lineHeight: 19.6
+    },
+
+    micro: {
+        fontFamily: fontFamilies.bold,
+        fontSize: 12,
+        fontStyle: 'normal',
+        fontWeight: '700',
+        lineHeight: 15.6,
+        letterSpacing: 0.24
+    }
+};
+
+export {
+    fontFamilies,
+    typography
+};
