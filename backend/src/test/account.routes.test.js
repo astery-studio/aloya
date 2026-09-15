89fc462ab67c0dedc8d56c70c380226950332f52
Kaylanne <kaylannesatiro@icloud.com>
2026-09-14 22:17:10 -0300

    fix: correção import test

--- backend/src/test/account.routes.test.js
+++ backend/src/test/account.routes.test.js
@@ -2,7 +2,7 @@
 const test = require('node:test')
 const assert = require('node:assert/strict')
 
-const { criarAccountRoutes } = require('../src/routes/account.routes')
+const { criarAccountRoutes } = require('../routes/account.routes')
 
 //Testes para as rotas de conta
 test(
