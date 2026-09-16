e355a80498aeb501fe8bcbfd2e00a3a417bd3b02
Kaylanne <kaylannesatiro@icloud.com>
2026-09-15 22:46:12 -0300

    fix: correção de importação pra ES Module

--- backend/test/account.validator.test.js
+++ backend/test/account.validator.test.js
@@ -2,8 +2,8 @@
 import test from 'node:test'
 import assert from 'node:assert/strict'
 
-const { criarAccountValidator } = require('../src/validators/account.validator');
-const dateUtils = require('../src/utils/date.utils');
+import { criarAccountValidator } from '../src/validators/account.validator.js'
+import * as dateUtils from '../src/utils/date.utils.js'
 
 //Funcao para criar um mock do validator de conta
 function criarValidator() {
