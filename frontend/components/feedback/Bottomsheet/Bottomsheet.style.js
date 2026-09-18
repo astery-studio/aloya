fe687c5106bb935c73a94bf3e3995427fc865b18
Kaylanne <kaylannesatiro@icloud.com>
2026-09-18 03:14:14 -0300

    feat: Bottomsheet

---
+++ frontend/components/feedback/Bottomsheet/Bottomsheet.style.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native'
+import { tema } from '../../theme'
+
+const estilos = StyleSheet.create({
+    tela: {
+        flex: 1,
+        justifyContent: 'flex-end'
+    },
+
+    fundo: {
+        ...StyleSheet.absoluteFillObject,
+        backgroundColor:
+            `${tema.cores.neutras.textoPrincipalClaro}66`
+    },
+
+    painel: {
+        maxHeight: '90%',
+        paddingBottom: tema.espacamentos.grande,
+        backgroundColor:
+            tema.cores.neutras.superficieClara,
+        borderTopLeftRadius:
+            tema.radius.bottomSheet,
+        borderTopRightRadius:
+            tema.radius.bottomSheet,
+        ...tema.shadows.sheet
+    }
+})
+
+export { estilos }
