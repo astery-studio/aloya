52229fecd285cb7587d8e8c6aab7bc884ccaa095
Kaylanne <kaylannesatiro@icloud.com>
2026-09-18 03:19:47 -0300

    feat: Bottomsheet Layout

---
+++ frontend/layouts/BottomSheet/BottomSheetLayout.style.js
@@ -0,0 +1,68 @@
+import { StyleSheet } from 'react-native'
+import { tema, fontFamilies } from '../theme'
+
+const estilos = StyleSheet.create({
+    container: {
+        flexShrink: 1
+    },
+
+    cabecalhoComAlca: {
+        alignItems: 'center',
+        paddingTop: 12,
+        paddingHorizontal:
+            tema.espacamentosLayout.margemHorizontalTela
+    },
+
+    alca: {
+        width: 40,
+        height: 4,
+        borderRadius: 2,
+        backgroundColor:
+            tema.cores.neutras.bordaClara
+    },
+
+    tituloCentral: {
+        marginTop: 24,
+        color:
+            tema.cores.neutras.textoPrincipalClaro,
+        fontFamily: fontFamilies.bold,
+        fontSize: 18,
+        lineHeight: 27,
+        textAlign: 'center'
+    },
+
+    cabecalhoComFechar: {
+        minHeight: 62,
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingTop: 12,
+        paddingHorizontal:
+            tema.espacamentosLayout.margemHorizontalTela
+    },
+
+    tituloEsquerda: {
+        flex: 1,
+        color:
+            tema.cores.neutras.textoPrincipalClaro,
+        fontFamily: fontFamilies.bold,
+        fontSize: 18,
+        lineHeight: 27
+    },
+
+    botaoFechar: {
+        width: 40,
+        height: 40,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    conteudo: {
+        flexShrink: 1,
+        paddingHorizontal:
+            tema.espacamentosLayout.margemHorizontalTela
+    }
+})
+
+const corIconeFechar = tema.cores.neutras.textoSecundarioClaro
+
+export { estilos, corIconeFechar }
