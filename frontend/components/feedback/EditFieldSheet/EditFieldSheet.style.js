fda2e5f799a9485ab5babbb0d741ae3372cff408
Kaylanne <kaylannesatiro@icloud.com>
2026-09-18 13:47:44 -0300

    fix: correção EditFielSheet

--- frontend/components/feedback/EditFieldSheet/EditFieldSheet.style.js
+++ frontend/components/feedback/EditFieldSheet/EditFieldSheet.style.js
@@ -3,31 +3,61 @@ import { tema } from '../../../theme'
 
 const estilos = StyleSheet.create({
     campo: {
+        ...tema.typography.bodyDefault,
         width: '100%',
-        minHeight: 56,
-        alignSelf: 'stretch',
+        height: 56,
         marginTop: 24,
         paddingHorizontal: 16,
         paddingVertical: 0,
         borderRadius: 12,
         borderWidth: 1.41,
         borderColor: tema.cores.marca.secundaria,
-        backgroundColor:
-            tema.cores.neutras.fundoClaro,
-        color:
-            tema.cores.neutras.textoPrincipalClaro,
+        backgroundColor: tema.cores.neutras.fundoClaro,
+        color: tema.cores.neutras.textoPrincipalClaro,
         textAlignVertical: 'center',
-        ...tema.typography.bodyDefault
+        includeFontPadding: false
     },
 
-    campoData: {
-        textAlign: 'center'
+    caixaData: {
+        width: '100%',
+        height: 56,
+        marginTop: 24,
+        paddingHorizontal: 16,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 12,
+        borderWidth: 1.41,
+        borderColor: tema.cores.marca.secundaria,
+        backgroundColor: tema.cores.neutras.fundoClaro
+    },
+
+    parteData: {
+        ...tema.typography.bodyDefault,
+        height: 52,
+        padding: 0,
+        color: tema.cores.neutras.textoPrincipalClaro,
+        textAlignVertical: 'center',
+        includeFontPadding: false
+    },
+
+    diaMes: {
+        width: 30
+    },
+
+    ano: {
+        width: 54
+    },
+
+    barraData: {
+        ...tema.typography.bodyDefault,
+        color: tema.cores.neutras.textoPrincipalClaro
     },
 
     erro: {
+        ...tema.typography.caption,
         marginTop: tema.espacamentos.pequeno,
-        color: tema.cores.feedback.erro,
-        ...tema.typography.caption
+        color: tema.cores.feedback.erro
     },
 
     espacoBotaoSalvar: {
