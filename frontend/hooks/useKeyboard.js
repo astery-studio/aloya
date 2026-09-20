105fc720026e5ee25d59af5994c5242a4dfe2be9
Carla Cristina <carlacristina130523@gmail.com>
2026-09-20 11:45:27 -0300

    feat(hooks): acompanhar estado do teclado

---
+++ frontend/hooks/useKeyboard.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import { Keyboard } from 'react-native';
+
+function useKeyboard() {
+    const [visivel, setVisivel] = useState(false);
+    const [altura, setAltura] = useState(0);
+
+    useEffect(() => {
+        const aoMostrar = Keyboard.addListener('keyboardDidShow', (evento) => {
+            setVisivel(true);
+            setAltura(evento.endCoordinates.height);
+        });
+        const aoOcultar = Keyboard.addListener('keyboardDidHide', () => {
+            setVisivel(false);
+            setAltura(0);
+        });
+
+        return () => {
+            aoMostrar.remove();
+            aoOcultar.remove();
+        };
+    }, []);
+
+    return { visivel, altura, fecharTeclado: Keyboard.dismiss };
+}
+
+export { useKeyboard };
