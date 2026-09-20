e28eeda09b4842d804cde717f241a08b3c09b6dd
Carla Cristina <carlacristina130523@gmail.com>
2026-09-20 10:49:11 -0300

    feat(modal): limitar variantes simples e alerta

---
+++ frontend/components/feedback/AlertModal.jsx
@@ -0,0 +1,5 @@
+import AppModal from './AppModal';
+
+export default function AlertModal(props) {
+    return <AppModal {...props} variante="alerta" />;
+}
--- frontend/components/feedback/SimpleModal.jsx
+++ frontend/components/feedback/SimpleModal.jsx
@@ -7,6 +7,7 @@ export default function SimpleModal({
 }) {
     return (
         <AppModal
+            variante="simples"
             visivel={visivel}
             aoFechar={aoFechar}
             icone={icone}
